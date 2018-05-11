c70fc70d89dc61b53246f72ce9bf7a58b03fe035
wangbuwu <469151282@qq.com>
2018-05-11 15:56:02 +0800

    ...

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+var Moment = require("../../utils/moment.js");
+var DATE_LIST = [];
+var DATE_YEAR = new Date().getFullYear()
+var DATE_MONTH = new Date().getMonth() + 1
+var DATE_DAY = new Date().getDate()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    checkInDate:"",
+    checkOutDate:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //设缓存缓存起来的日期
+    wx.setStorage({
+      key: 'ROOM_SOURCE_DATE',
+      data: {
+        checkInDate: Moment(new Date()).format('YYYY-MM-DD'),
+        checkOutDate: Moment(new Date()).add(1, 'day').format('YYYY-MM-DD')
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let getDate = wx.getStorageSync("ROOM_SOURCE_DATE");
+    this.setData({
+      checkInDate: getDate.checkInDate,
+      checkOutDate: getDate.checkOutDate
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
